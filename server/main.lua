dd7b7e57e64766a3b65248b5f73de3d876d3d62e
sTREXiia <105889822+sTREXiia@users.noreply.github.com>
2022-05-27 15:57:30 +0430

    changing cloth menu

--- server/main.lua
+++ server/main.lua
@@ -168,11 +168,22 @@ QBCore.Functions.CreateCallback("qb-multicharacter:server:setupCharacters", func
     end)
 end)
 
-QBCore.Functions.CreateCallback("qb-multicharacter:server:getSkin", function(_, cb, cid)
-    local result = MySQL.query.await('SELECT * FROM playerskins WHERE citizenid = ? AND active = ?', {cid, 1})
-    if result[1] ~= nil then
-        cb(result[1].model, result[1].skin)
+-- QBCore.Functions.CreateCallback("qb-multicharacter:server:getSkin", function(_, cb, cid)
+--     local result = MySQL.query.await('SELECT * FROM playerskins WHERE citizenid = ? AND active = ?', {cid, 1})
+--     if result[1] ~= nil then
+--         cb(result[1].model, result[1].skin)
+--     else
+--         cb(nil)
+--     end
+-- end) replaced by sTREXia
+QBCore.Functions.CreateCallback("qb-multicharacter:server:getSkin", function(source, cb, cid)
+    local result = MySQL.query.await('SELECT * FROM players WHERE citizenid = ?', {cid})
+    local PlayerData = result[1]
+    PlayerData.model = json.decode(PlayerData.skin)
+    if PlayerData.skin ~= nil then
+        cb(PlayerData.skin, PlayerData.model.model)
     else
         cb(nil)
     end
 end)
+
